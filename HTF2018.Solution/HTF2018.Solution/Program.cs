a5796269795e605272ec073e161fadcc9d901dad
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2018-12-21 14:22:59 +0100

    Initial project commit

---
+++ HTF2018.Solution/HTF2018.Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTF2018.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
