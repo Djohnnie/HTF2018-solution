bb30dcd66334c49d61b66f669c03b32f6eda24ad
BruceVandeweyer <bruce.vandeweyer@involved-it.be>
2018-12-21 17:25:01 +0100

    Challenge08

--- HTF2018.Solution/HTF2018.Solution/Logic/Challenge08.cs
+++ HTF2018.Solution/HTF2018.Solution/Logic/Challenge08.cs
@@ -1,5 +1,7 @@
-﻿using HTF2018.Solution.Model;
+﻿using System;
+using HTF2018.Solution.Model;
 using System.Collections.Generic;
+using System.Text;
 
 namespace HTF2018.Solution.Logic
 {
@@ -7,7 +9,19 @@ namespace HTF2018.Solution.Logic
     {
         public List<Value> Solve(List<Value> inputValues)
         {
-            return new List<Value> { };
+            int numOfBytes = inputValues[0].Data.Length / 8;
+            byte[] bytes = new byte[numOfBytes];
+            for (int i = 0; i < numOfBytes; ++i)
+            {
+                bytes[i] = Convert.ToByte(inputValues[0].Data.Substring(8 * i, 8), 2);
+            }
+            
+            var result = Encoding.ASCII.GetString(bytes);
+
+            return new List<Value>
+            {
+                new Value {Name="decoded", Data=$"{result}"}
+            };
         }
     }
 }
