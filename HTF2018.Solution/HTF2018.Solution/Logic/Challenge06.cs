03e7e133796411964581b388b3d472aa646273c2
BruceVandeweyer <bruce.vandeweyer@involved-it.be>
2018-12-21 16:19:49 +0100

    Challenge06

--- HTF2018.Solution/HTF2018.Solution/Logic/Challenge06.cs
+++ HTF2018.Solution/HTF2018.Solution/Logic/Challenge06.cs
@@ -1,5 +1,7 @@
-﻿using HTF2018.Solution.Model;
+﻿using System;
+using HTF2018.Solution.Model;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace HTF2018.Solution.Logic
 {
@@ -7,7 +9,28 @@ namespace HTF2018.Solution.Logic
     {
         public List<Value> Solve(List<Value> inputValues)
         {
-            return new List<Value> { };
+            int result = 0;
+            var startDate = DateTime.Parse(inputValues[0].Data, new CultureInfo("en-us"));
+            var endDate = DateTime.Parse(inputValues[1].Data, new CultureInfo("en-us"));
+            Enum.TryParse(inputValues[2].Data, out DayOfWeek theDay);
+
+            var timeSpan = endDate - startDate;
+
+            for (int day = 0; day <= timeSpan.TotalDays; day++)
+            {
+                var date = startDate.AddDays(day);
+
+                if (date.DayOfWeek == theDay)
+                {
+                    result++;
+                }
+            }
+
+            return new List<Value>
+            {
+                new Value {Name = "count", Data = $"{result}"}
+            };
+
         }
     }
 }
