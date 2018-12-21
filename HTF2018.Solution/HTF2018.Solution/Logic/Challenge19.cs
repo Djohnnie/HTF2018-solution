35b36b86a846ca538d71e0a6a0a9adb0ed0b100f
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2018-12-21 15:38:28 +0100

    Added empty challenge solution classes

---
+++ HTF2018.Solution/HTF2018.Solution/Logic/Challenge19.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using HTF2018.Solution.Model;
+
+namespace HTF2018.Solution.Logic
+{
+    public class Challenge19 : IChallenge
+    {
+        public List<Value> Solve(List<Value> inputValues)
+        {
+            return new List<Value> { };
+        }
+    }
+}
